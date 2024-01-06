ef1dbb14f0a350b86261331dba99459edc06b955
Mazapan39 <126164380+Mazapan39@users.noreply.github.com>
2024-01-06 18:51:15 +0100

    s

--- Micro02 - Ahora es personal/Professors-Projectes.php
+++ Micro02 - Ahora es personal/Professors-Projectes.php
@@ -1,25 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My Webpage</title>
-  <link rel="stylesheet" href="css/professores.css">
-  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-  <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Webpage</title>
+    <link rel="stylesheet" href="css/professores.css">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    
+
     <header class="initial">
         <div class="logo">
             <img style="border-radius: 50%; width: 100px;" src="img/logo.png" alt="">
             <h1>PROFESSORS</h1>
         </div>
         <nav>
-        <form method="post">
+            <form method="post">
                 <button id="home" name="home">Home</button>
-        </form>        
-        <h2>(NombreProfe)</h2>
+            </form>
+            <h2>(NombreProfe)</h2>
         </nav>
     </header>
     <div class="header">
@@ -33,34 +35,42 @@
         <div class="formulari">
             <div class="l-1">
                 <div style="display: flex; gap: 47px;">
-                <label for="">Nom</label>
-                <input type="text">
-            </div><div>
-                <label for="">Modul</label>
-                <input type="text">
-            </div>
-                <a href="">CREAR</a>
+                    <label for="">Nom</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label for="">Modul</label>
+                    <input type="text">
+                </div>
+                <form method="post">
+                    <button id="crear" name="crear">Crear</button>
+                </form>
             </div>
             <div class="l-2">
                 <div style="display: flex; align-items: top; gap: 10px;">
-                <label for="">Descripcio</label>
-                <input type="text">
-            </div>
+                    <label for="">Descripcio</label>
+                    <input type="text">
+                </div>
                 <div class="escollir">
                     <div class="b-skills">
                         <p>Skills</p>
-                        <a href="">Escollir</a>
+                        <form method="post">
+                            <button id="escollir" name="escollir">Escollir</button>
+                        </form>
                     </div>
                     <div class="b-alumnes">
                         <p>Alumnes</p>
-                        <a href="">Escollir</a>
+                        <form method="post">
+                            <button id="escollir" name="escollir">Escollir</button>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
-</div>
-        <footer>
-            <p>Derechos de imagen @Chamous</p>
-        </footer>
-    </body>
-    </html>
+    </div>
+    <footer>
+        <p>Derechos de imagen @Chamous</p>
+    </footer>
+</body>
+
+</html>
