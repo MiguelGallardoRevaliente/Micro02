63a628a1cc92632c99ba7833e35e5374f75f3ec2
Mazapan39 <126164380+Mazapan39@users.noreply.github.com>
2024-01-07 18:23:01 +0100

    ds

--- Micro02 - Ahora es personal/Professors-Estudiants.php
+++ Micro02 - Ahora es personal/Professors-Estudiants.php
@@ -7,6 +7,7 @@
   <link rel="stylesheet" href="css/professores.css">
   <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
   <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="js/professores.js">
 </head>
 <body>
     
@@ -17,15 +18,7 @@
         </div>
         <nav>
         <button id="home" name="home">Home</button>
-        <?php
-            include "connexio.php";
-            $sql = "SELECT nom FROM professors where id_professor = 1";
-            $nom = mysqli_query($conn, $sql);
-            foreach ($nom as $string) {
-                $valor = implode($string);
-                echo "<h2>" . $valor . "</h2>";
-            }
-            ?>
+       
         </nav>
     </header>
     <div class="header">
@@ -43,7 +36,7 @@
             </div>
             <div class="botones">
             <form method="post">
-                    <button id="crear" name="crear">Crear</button> 
+                    <button id="crear" onclick="mostrarCrear()" name="crear">Crear</button> 
                     <button id="modificar" name="modificar">Modificar</button>
                     <button id="borrar" name="borrar">Borrar</button>
                     <button class="importar" id="importar" name="importar">Importar</button>
@@ -58,6 +51,10 @@
         </div>
         <div class="estudiant"></div>
 
+    </div>
+        <div class="prueba">
+             CREAR ESTUDIANTS
+       
     </div>
     <footer>
         <p>Derechos de imagen @Chamous</p>
---
+++ Micro02 - Ahora es personal/js/professores.js
@@ -0,0 +1,5 @@
+//mostrar un div con id crear al darle a un boton
+function mostrarCrear() {
+    var div = document.getElementById("prueba");
+    div.style.display = "block";
+}
