029657d81abe6a0118a68ae6a620ef63aeed6ae7
Mazapan39 <126164380+Mazapan39@users.noreply.github.com>
2024-01-09 10:34:01 +0100

    js

--- Micro02 - Ahora es personal/js/professores.js
+++ Micro02 - Ahora es personal/js/professores.js
@@ -1,6 +1,11 @@
 let prueba = document.getElementById("prueba");
 let all = document.getElementsByClassName("Professors_Estudiants");
+let skills = document.getElementById("skills");
 
 function mostrarCrear() {
     prueba.style.display = "block";
+}
+
+function mostrarSkills() {
+    skills.style.display = "block";
 }
